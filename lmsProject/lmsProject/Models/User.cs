787157d6b0ea4849d514c024fcf064ec98aa0384
hoang9x000 <hoang9x000@gmail.com>
2019-11-27 01:44:56 +0700

    connect database

---
+++ lmsProject/lmsProject/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace lmsProject.Models
+{
+    public class User
+    {
+        [Key]
+        public string Mathe { get; set; }
+        public string Matkhau { get; set; }
+        public string Hoten { get; set; }
+        public Boolean Gioitinh { get; set; }
+        public DateTime Ngaysinh { get; set; }
+        public string Diachi { get; set; }
+        public DateTime Ngaydangki { get; set; }
+        public DateTime Ngayhethan { get; set; }
+        public Boolean isAdmin { get; set; }
+    }
+}
---
+++ lmsProject/lmsProject/Models/myDataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lmsProject.Models
+{
+    public class myDataContext: DbContext
+    {
+        public myDataContext(DbContextOptions<myDataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
