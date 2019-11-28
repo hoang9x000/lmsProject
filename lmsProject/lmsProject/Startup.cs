5bc663d31205e5ca7633853421efd6da10b9fedd
hoang9x000 <hoang9x000@gmail.com>
2019-11-29 03:54:11 +0700

    Fix bug Navigation properties

--- lmsProject/lmsProject/Startup.cs
+++ lmsProject/lmsProject/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
 namespace lmsProject
@@ -28,13 +29,17 @@ namespace lmsProject
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddJsonOptions(opt =>
+            {
+                opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            })
                 .AddJsonOptions(options => {
                 var resolver = options.SerializerSettings.ContractResolver;
                 if (resolver != null)
                     (resolver as DefaultContractResolver).NamingStrategy = null;
             }); ;
 
-            services.AddDbContext<lmsContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+            services.AddDbContext<lmsContext>(options => options.UseLazyLoadingProxies().UseSqlServer(Configuration.GetConnectionString("DevConnection"))) ;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
