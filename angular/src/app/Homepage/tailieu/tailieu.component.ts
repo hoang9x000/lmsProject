fa6452ed43a8a3b916d8540fde2dff517d191b37
Minhchien0204 <48354680+Minhchien0204@users.noreply.github.com>
2019-11-29 16:44:15 +0700

    chia route trang hone

---
+++ angular/src/app/Homepage/tailieu/tailieu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tailieu',
+  templateUrl: './tailieu.component.html',
+  styleUrls: ['./tailieu.component.css']
+})
+export class TailieuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,12 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Header1Component } from './Homepage/header1/header1.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ThongtinComponent } from './Homepage/thongtin/thongtin.component';
+import { QuydinhComponent } from './Homepage/quydinh/quydinh.component';
+import { GioithieuComponent } from './Homepage/gioithieu/gioithieu.component';
+import { DangnhapComponent } from './dangnhap/dangnhap.component';
+import { TailieuComponent } from './Homepage/tailieu/tailieu.component';
+import { ChangepassComponent } from './changepass/changepass.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Header1Component,
+    FooterComponent,
+    HomeComponent,
+    ThongtinComponent,
+    QuydinhComponent,
+    GioithieuComponent,
+    DangnhapComponent,
+    TailieuComponent,
+    ChangepassComponent,
   ],
   imports: [
     BrowserModule,
